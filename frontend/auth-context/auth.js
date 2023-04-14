6dcbe9b9b0810ad6c6290b45b6ca2de5ac37ecae
Architsharma7 <architsharma7911@gmail.com>
2023-04-15 01:46:03 +0900

    fix:auth testing

--- frontend/auth-context/auth.js
+++ frontend/auth-context/auth.js
@@ -6,6 +6,7 @@ import {
   SafeAuthProviderType,
   SafeAuthEvents,
 } from "@safe-global/auth-kit";
+import { publicPaths } from "../constants/publicpath";
 
 const AuthContext = createContext();
 
@@ -25,7 +26,7 @@ export function AuthProvider({ children }) {
 
   useEffect(() => {
     const authCheck = () => {
-      if (!user && !publicPaths.includes(router.asPath.split("?")[0])) {
+      if (!currentUser && !publicPaths.includes(router.asPath.split("?")[0])) {
         setAuthorized(false);
         // dispatch(setRedirectLink({ goto: router.asPath }));
         void router.push({
@@ -50,7 +51,7 @@ export function AuthProvider({ children }) {
   }, [router, router.events, currentUser]);
 
   const intializeAuthKit = async () => {
-    const safeAuthKit = await SafeAuthKit.init(SafeAuthProviderType.Web3Auth, {
+    const safeAuthKit = await SafeAuthKit.init(SafeAuthProviderType, {
       chainId: "0x5",
       authProviderConfig: {
         rpcTarget:
@@ -90,39 +91,39 @@ export function AuthProvider({ children }) {
   };
 
   useEffect(() => {
-    // (async () => {
-    //   const safeAuthKit = await SafeAuthKit.init(
-    //     SafeAuthProviderType.Web3Auth,
-    //     {
-    //       chainId: "0x5",
-    //       authProviderConfig: {
-    //         rpcTarget:
-    //           "https://polygon-mumbai.g.alchemy.com/v2/bZFiL-IFAMe4QAh9Q30gDQ7m1vxEss4u", // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
-    //         clientId:
-    //           "BI2SkFVRuQr8TqLoicvYRQivxyw8HL7FtfKok4VQXKhQ4V38pop3yLJhFQEphRfee3bGNG5u_wqfwePZsijnpcg", // Add your client id. Get it from the Web3Auth dashboard
-    //         network: "testnet" | "mainnet", // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
-    //         theme: "light" | "dark", // The theme to use for the Web3Auth modal
-    //         modalConfig: {
-    //           // The modal config is  optional and it's used to customize the Web3Auth modal
-    //           // Check the Web3Auth documentation for more info: https://web3auth.io/docs/sdk/web/modal/whitelabel#initmodal
-    //         },
-    //       },
-    //     }
-    //   );
+    (async () => {
+      const safeAuthKit = await SafeAuthKit.init(
+        SafeAuthProviderType.Web3Auth,
+        {
+          chainId: "0x5",
+          authProviderConfig: {
+            rpcTarget:
+              "https://polygon-mumbai.g.alchemy.com/v2/bZFiL-IFAMe4QAh9Q30gDQ7m1vxEss4u", // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
+            clientId:
+              "BI2SkFVRuQr8TqLoicvYRQivxyw8HL7FtfKok4VQXKhQ4V38pop3yLJhFQEphRfee3bGNG5u_wqfwePZsijnpcg", // Add your client id. Get it from the Web3Auth dashboard
+            network: "testnet" | "mainnet", // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
+            theme: "light" | "dark", // The theme to use for the Web3Auth modal
+            modalConfig: {
+              // The modal config is  optional and it's used to customize the Web3Auth modal
+              // Check the Web3Auth documentation for more info: https://web3auth.io/docs/sdk/web/modal/whitelabel#initmodal
+            },
+          },
+        }
+      );
 
-    //   safeAuthKit.subscribe(SafeAuthEvents.SIGNED_IN, () => {
-    //     console.log("User is authenticated");
-    //   });
+      safeAuthKit.subscribe(SafeAuthEvents.SIGNED_IN, () => {
+        console.log("User is authenticated");
+      });
 
-    //   safeAuthKit.subscribe(SafeAuthEvents.SIGNED_OUT, () => {
-    //     console.log("User is not authenticated");
-    //   });
+      safeAuthKit.subscribe(SafeAuthEvents.SIGNED_OUT, () => {
+        console.log("User is not authenticated");
+      });
 
-    //   setSafeAuthKit(safeAuthKit);
-    //   console.log(safeAuthKit);
-    // })();
+      setSafeAuthKit(safeAuthKit);
+      console.log(safeAuthKit);
+    })();
 
-    intializeAuthKit();
+    // intializeAuthKit();
   }, []);
 
   const value = {
