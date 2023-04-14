4b927c0bb62713de0a814577fcc77e0c1c9bb4b4
Architsharma7 <architsharma7911@gmail.com>
2023-04-15 01:15:51 +0900

    fix: auth context

--- frontend/auth-context/auth.js
+++ frontend/auth-context/auth.js
@@ -1,6 +1,7 @@
 import React, { createContext } from "react";
 import { useContext, useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
+import { SafeAuthKit, SafeAuthProviderType } from '@safe-global/auth-kit';
 
 const AuthContext = createContext();
 
@@ -13,6 +14,32 @@ export function AuthProvider({ children }) {
   const userInfo = useRef();
   const router = useRouter();
   const [authorized, setAuthorized] = useState(false);
+
+  useEffect(() => {
+    const authCheck = () => {
+      if (!user && !publicPaths.includes(router.asPath.split("?")[0])) {
+        setAuthorized(false);
+        // dispatch(setRedirectLink({ goto: router.asPath }));
+        void router.push({
+          pathname: "/hello",
+        });
+      } else {
+        setAuthorized(true);
+      }
+    };
+
+    authCheck();
+
+    const preventAccess = () => setAuthorized(false);
+
+    router.events.on("routeChangeStart", preventAccess);
+    router.events.on("routeChangeComplete", authCheck);
+
+    return () => {
+      router.events.off("routeChangeStart", preventAccess);
+      router.events.off("routeChangeComplete", authCheck);
+    };
+  }, [router, router.events, currentUser]);
 
   const value = {
   };
