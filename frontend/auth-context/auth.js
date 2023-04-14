76b4888d6a035e287835245db78f98a9af051790
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-04-15 01:28:52 +0900

    feat: Added Safe Auth Kit method

--- frontend/auth-context/auth.js
+++ frontend/auth-context/auth.js
@@ -1,7 +1,11 @@
 import React, { createContext } from "react";
 import { useContext, useState, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
-import { SafeAuthKit, SafeAuthProviderType } from '@safe-global/auth-kit';
+import {
+  SafeAuthKit,
+  SafeAuthProviderType,
+  SafeAuthEvents,
+} from "@safe-global/auth-kit";
 
 const AuthContext = createContext();
 
@@ -11,6 +15,8 @@ export const useAuth = () => {
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
+  const [safeAuthKit, setSafeAuthKit] = useState();
+
   const userInfo = useRef();
   const router = useRouter();
   const [authorized, setAuthorized] = useState(false);
@@ -41,7 +47,85 @@ export function AuthProvider({ children }) {
     };
   }, [router, router.events, currentUser]);
 
+  const intializeAuthKit = async () => {
+    const safeAuthKit = await SafeAuthKit.init(SafeAuthProviderType.Web3Auth, {
+      chainId: "0x5",
+      authProviderConfig: {
+        rpcTarget:
+          "https://polygon-mumbai.g.alchemy.com/v2/bZFiL-IFAMe4QAh9Q30gDQ7m1vxEss4u", // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
+        clientId:
+          "BI2SkFVRuQr8TqLoicvYRQivxyw8HL7FtfKok4VQXKhQ4V38pop3yLJhFQEphRfee3bGNG5u_wqfwePZsijnpcg", // Add your client id. Get it from the Web3Auth dashboard
+        network: "testnet" | "mainnet", // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
+        theme: "light" | "dark", // The theme to use for the Web3Auth modal
+        modalConfig: {
+          // The modal config is  optional and it's used to customize the Web3Auth modal
+          // Check the Web3Auth documentation for more info: https://web3auth.io/docs/sdk/web/modal/whitelabel#initmodal
+        },
+      },
+    });
+
+    safeAuthKit.subscribe(SafeAuthEvents.SIGNED_IN, () => {
+      console.log("User is authenticated");
+    });
+
+    safeAuthKit.subscribe(SafeAuthEvents.SIGNED_OUT, () => {
+      console.log("User is not authenticated");
+    });
+
+    setSafeAuthKit(safeAuthKit);
+    console.log(safeAuthKit);
+  };
+
+  const getUser = async () => {
+    if (!safeAuthKit) {
+      await intializeAuthKit();
+    }
+    const eoaAddress = safeAuthKit.getProvider();
+    if (eoaAddress) {
+      setCurrentUser(eoaAddress);
+      setAuthorized(true);
+    }
+  };
+
+  useEffect(() => {
+    // (async () => {
+    //   const safeAuthKit = await SafeAuthKit.init(
+    //     SafeAuthProviderType.Web3Auth,
+    //     {
+    //       chainId: "0x5",
+    //       authProviderConfig: {
+    //         rpcTarget:
+    //           "https://polygon-mumbai.g.alchemy.com/v2/bZFiL-IFAMe4QAh9Q30gDQ7m1vxEss4u", // Add your RPC e.g. https://goerli.infura.io/v3/<your project id>
+    //         clientId:
+    //           "BI2SkFVRuQr8TqLoicvYRQivxyw8HL7FtfKok4VQXKhQ4V38pop3yLJhFQEphRfee3bGNG5u_wqfwePZsijnpcg", // Add your client id. Get it from the Web3Auth dashboard
+    //         network: "testnet" | "mainnet", // The network to use for the Web3Auth modal. Use 'testnet' while developing and 'mainnet' for production use
+    //         theme: "light" | "dark", // The theme to use for the Web3Auth modal
+    //         modalConfig: {
+    //           // The modal config is  optional and it's used to customize the Web3Auth modal
+    //           // Check the Web3Auth documentation for more info: https://web3auth.io/docs/sdk/web/modal/whitelabel#initmodal
+    //         },
+    //       },
+    //     }
+    //   );
+
+    //   safeAuthKit.subscribe(SafeAuthEvents.SIGNED_IN, () => {
+    //     console.log("User is authenticated");
+    //   });
+
+    //   safeAuthKit.subscribe(SafeAuthEvents.SIGNED_OUT, () => {
+    //     console.log("User is not authenticated");
+    //   });
+
+    //   setSafeAuthKit(safeAuthKit);
+    //   console.log(safeAuthKit);
+    // })();
+
+    intializeAuthKit();
+  }, []);
+
   const value = {
+    safeAuthKit,
+    intializeAuthKit,
   };
 
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
--- frontend/pages/onboarding.js
+++ frontend/pages/onboarding.js
@@ -1,13 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useAuth } from "../auth-context/auth";
 
 const Onboarding = () => {
+  const { safeAuthKit: safeAuth, intializeAuthKit } = useAuth();
+
+  useEffect(() => {
+    if (!safeAuth) {
+      intializeAuthKit();
+      console.log("Intializing Auth Kit");
+    }
+  }, []);
+
+  const login = async () => {
+    try {
+      console.log("Loginigg in ");
+      console.log(safeAuth);
+      if (!safeAuth) {
+        console.log("SafeAuthNot found");
+        return;
+      }
+      const response = await safeAuth.signIn();
+      console.log("SIGN IN RESPONSE: ", response);
+
+      setsafeAuthSigninResponse(response);
+      setProvider(safeAuth.getProvider());
+      setIsLoggedIn(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const logout = async () => {
+    console.log(safeAuth);
+    if (!safeAuth) {
+      console.log("SafeAuthNot found");
+      return;
+    }
+
+    await safeAuth.signOut();
+
+    setProvider(null);
+    setsafeAuthSigninResponse(null);
+  };
+
   return (
     <div className="bg-stone-900 w-screen h-screen">
       <div className="justify-center flex flex-col mx-auto">
         <div className="bg-stone-900">
-            <div className="mt-20 flex justify-center text-4xl">
-                <p className="text-emerald-500">Smooth Wallet</p>
-            </div>
+          <div className="mt-20 flex justify-center text-4xl">
+            <p className="text-emerald-500">Smooth Wallet</p>
+          </div>
         </div>
       </div>
     </div>
