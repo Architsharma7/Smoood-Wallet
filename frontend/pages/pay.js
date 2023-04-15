ca4292dc6c99fae56f1b872b350168682326bf00
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-04-15 16:14:21 +0900

    feat: Safe Relay kit added in Pay

--- frontend/pages/pay.js
+++ frontend/pages/pay.js
@@ -1,7 +1,62 @@
 import React from "react";
+import { ethers } from "ethers";
+import {
+  GelatoRelayAdapter,
+  MetaTransactionOptions,
+} from "@safe-global/relay-kit";
+import Safe from "@safe-global/safe-core-sdk";
+import {
+  MetaTransactionData,
+  OperationType,
+} from "@safe-global/safe-core-sdk-types";
+
+const GELATO_RELAY_API_KEY = process.env.NEXT_PUBLIC_GELATO_RELAY_API_KEY;
 
 const Pay = () => {
+  const {} = useAuth();
+
+  const [safeAddress, setSafeAddress] = useState();
+  const chainId = 5;
+  const options = {
+    gasLimit: ethers.BigNumber.from(gasLimit),
+    isSponsored: true,
+  };
+
+  const prepareTransactionData = () => {};
+
   /// Building the tx
+  const sendTransaction1Balance = async () => {
+    const relayAdapter = new GelatoRelayAdapter(GELATO_RELAY_API_KEY);
+
+    // we need to get the encoded tx data
+    const relayTransaction = {
+      target: targetContractAddress,
+      encodedTransaction: encodedTx,
+      chainId,
+      options,
+    };
+    const response = await relayAdapter.relayTransaction(relayTransaction);
+
+    console.log(
+      `Relay Transaction Task ID: https://relay.gelato.digital/tasks/status/${response.taskId}`
+    );
+  };
+
+  const sendTransactionSyncFee = async () => {
+    const relayAdapter = new GelatoRelayAdapter();
+
+    // we need to get the encoded tx data
+    const relayTransaction = {
+      target: targetContractAddress,
+      encodedTransaction: encodedTx,
+      chainId,
+    };
+    const response = await relayAdapter.relayTransaction(relayTransaction);
+
+    console.log(
+      `Relay Transaction Task ID: https://relay.gelato.digital/tasks/status/${response.taskId}`
+    );
+  };
   return (
     <div className="h-screen w-screen bg-white">
       <div className="flex flex-col h-[75%]">
