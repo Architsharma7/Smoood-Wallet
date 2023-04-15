6e4619001c11d435f3ff94a26cb38bc67d4cf054
Dhruv-2003 <dhruvagarwal2017@gmail.com>
2023-04-16 05:46:23 +0900

    feat: Contract deployed and constants added

---
+++ frontend/constants/contractData.js
@@ -0,0 +1,39 @@
+export const recoveryModuleContractAddress =
+  "0x2B74083B670009fA63e7CceC16A0400cc202f7c8";
+
+export const recoveryModuleABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "value",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "data",
+        type: "bytes",
+      },
+      {
+        internalType: "enum GnosisSafe.Operation",
+        name: "operation",
+        type: "uint8",
+      },
+    ],
+    name: "execTransactionFromModule",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "success",
+        type: "bool",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
